003cbdb85fba7c4e10870bae272e57374608deee
Erica Sampson <ericabcherry@gmail.com>
2024-07-12 11:18:01 -0400

    Initial Commit

---
+++ 100DS-P3-BetterRest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  100DS-P3-BetterRest
+//
+//  Created by Erica Sampson on 2024-07-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100DS-P3-BetterRest/_00DS_P3_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DS_P3_BetterRestApp.swift
+//  100DS-P3-BetterRest
+//
+//  Created by Erica Sampson on 2024-07-12.
+//
+
+import SwiftUI
+
+@main
+struct _00DS_P3_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
